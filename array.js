cb4e7fcf941def1c7fcb1abea26e49c030c10bd8
Aktonen <aku.tolonen1999@hotmail.fi>
2021-11-22 13:46:22 +0200

    finished exercises

--- array.js
+++ array.js
@@ -1,31 +1,48 @@
+//Harj1
 // var numbers = ["one", "two", "three", "four", "five"]
 
 // numbers.forEach(number => console.log(number))
 
+//Harj2
 // var prices = [20, 40, 35, 12, 100]
 
-// var discountedPrices = prices.map(price => price * 0.8)
+// var discountedPrices = prices.map(price => price * 0.2)
 // console.log(discountedPrices)
 
-// var names = ["John", "Joni", "John", "Matti", "Aku"]
+//Harj3
+// var names = ["John", "Jack", "Lisa", "John", "Sara"]
 
-// var namesChanged = names.map(name => {
-//     if (name == "John"){
-//         name == "Matti"
+// names = names.map( name => name === "John" ? "Matti" : name)
+
+// Toinen versio
+
+// names = names.map (name => {
+//     if(name === "John"){
+//         return "Matti"
 //     }
-//     return name
+//     return name;
 // })
 
 // console.log(names);
 
+//Harj4
 // var numbers = [1, 2, 3, 4, 0, -1, -2, -3]
 
 // var positiveNumbers = numbers.filter(number => number >= 0)
 
 // console.log(positiveNumbers);
 
-// var names = ["John", "Marry", "Tristana", "Tero"]
+//Harj5
+var names = ["John", "Marry", "Tristana", "Tero"]
 
 // var filteredNames = names.filter(name => name.length >= 8 && name[0] == "T")
 
 // console.log(filteredNames);
+
+//Harj6
+if (names.every(sana => sana.length > 0)) {
+    let pituudet = names.map(sana => sana.length)
+
+    let summa = pituudet.reduce((previous, current) => previous + current)
+    console.log(summa);
+}
