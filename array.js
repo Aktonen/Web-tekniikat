1249e8860b81381edea2569cc7538872e072a6f6
Aktonen <aku.tolonen1999@hotmail.fi>
2021-11-22 13:18:50 +0200

    Array manipulation exercises

---
+++ array.js
@@ -0,0 +1,31 @@
+// var numbers = ["one", "two", "three", "four", "five"]
+
+// numbers.forEach(number => console.log(number))
+
+// var prices = [20, 40, 35, 12, 100]
+
+// var discountedPrices = prices.map(price => price * 0.8)
+// console.log(discountedPrices)
+
+// var names = ["John", "Joni", "John", "Matti", "Aku"]
+
+// var namesChanged = names.map(name => {
+//     if (name == "John"){
+//         name == "Matti"
+//     }
+//     return name
+// })
+
+// console.log(names);
+
+// var numbers = [1, 2, 3, 4, 0, -1, -2, -3]
+
+// var positiveNumbers = numbers.filter(number => number >= 0)
+
+// console.log(positiveNumbers);
+
+// var names = ["John", "Marry", "Tristana", "Tero"]
+
+// var filteredNames = names.filter(name => name.length >= 8 && name[0] == "T")
+
+// console.log(filteredNames);
